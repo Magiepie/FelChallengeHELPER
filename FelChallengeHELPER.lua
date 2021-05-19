45403e8862bd987a5a31e1ad8aaca91e0b6b1230
Magiepie <iamthedustin@gmail.com>
2021-05-18 22:22:22 -0400

    Update FelChallengeHELPER.lua
    Items in Fel-Challenge shops now highlight in green if completed.
    Untested with tiers higher then 1 (im a noob)

--- FelChallengeHELPER.lua
+++ FelChallengeHELPER.lua
@@ -1,18 +1,72 @@
-function ToolTip()  
+local frame, events = CreateFrame("Frame"), {};
+function events:MERCHANT_SHOW(...)
+	if  MerchantNameText:GetText() == "Azzazel" then
+	   if MerchantItem1Name:GetText() == "Slaughterhouse" then
+			 FelChallengeShop()
+	   end
+	end
+end
+
+frame:SetScript("OnEvent", function(self, event, ...)
+	events[event](self, ...); 
+end);
+for k, v in pairs(events) do
+	frame:RegisterEvent(k);
+end
+
+
+
+function ToolTip(i)  
     local mytext1=_G["GameTooltipTextLeft1"] 
-    local ChalName=mytext1:GetText()
+	--local Challenge = Name:match(".*: (%a.*)")
+    local ChalName= mytext1:GetText()
     local mytext2=_G["GameTooltipTextLeft2"] 
     local Tier=mytext2:GetText()
+	local Tier = Tier:match("Tier (%d*)")
+
     if Tier and ChalName and IsChallenge(Tier,ChalName) then -- Fix lua errors By Szyler
         GameTooltipTextLeft1:SetText(ChalName.." -- Completed")
+
     end
 end
 
+function MerchantFrame_Update()
+	if ( MerchantFrame.selectedTab == 1 ) then
+		MerchantFrame_UpdateMerchantInfo();
+		FelChallengeShop()
+	else
+		MerchantFrame_UpdateBuybackInfo();
+	end
+	
+end
+
+function FelChallengeShop()
+	local FelTier = GetFelChalTier()
+	for i=1,10 do
+		local ChalName = _G["MerchantItem"..i.."Name"]:GetText();
+		if IsChallenge(FelTier,ChalName) then
+			local itemButton = _G["MerchantItem"..i.."ItemButton"];
+			local merchantButton = _G["MerchantItem"..i];
+			SetItemButtonNameFrameVertexColor(merchantButton, 0, 1, 0);
+			SetItemButtonSlotVertexColor(merchantButton, 0, 1, 0);
+			SetItemButtonNormalTextureVertexColor(itemButton, 0, 1.0, 0);
+		end
+	end
+	
+end
+
+function GetFelChalTier()
+	for i = 1,10 do
+		local IDNumber, Name, Points, Completed, Month, Day, Year, Description, Flags, Image, RewardText, isGuildAch = GetAchievementInfo(39990+i)
+		if Completed == false then return i end
+	end
+	return 1
+end
+
 function IsChallenge(Tier,ChalName)
-    local Tier = Tier:match("Tier (%d*)")
-    if Tier then -- Fix lua errors By Szyler
+    if Tier ~= nil then -- Fix lua errors By Szyler
         for i=1,25 do
-            local IDNumber, Name, Points, Completed, Month, Day, Year, Description, Flags, Image, RewardText, isGuildAch = GetAchievementInfo(80000+Tier, i) 
+            local IDNumber, Name, Points, Completed, Month, Day, Year, Description, Flags, Image, RewardText, isGuildAch = GetAchievementInfo(80000+tonumber(Tier), i) 
             local Challenge = Name:match(".*: (%a.*)")
             if Challenge == ChalName then 
                 return Completed
@@ -26,6 +80,7 @@ ItemRefTooltip:HookScript("OnTooltipSetItem", function(tooltip, ...) -- ItemRef 
     local itemLinkName=myItemText1:GetText()
     local mytext2=_G["ItemRefTooltipTextLeft2"] 
     local Tier=mytext2:GetText()
+	 local Tier = Tier:match("Tier (%d*)")
     if Tier and itemLinkName and IsChallenge(Tier,itemLinkName) then 
         ItemRefTooltipTextLeft1:SetText(itemLinkName.." -- Completed")
     end
@@ -34,7 +89,7 @@ ItemRefTooltip:HookScript("OnTooltipSetItem", function(tooltip, ...) -- ItemRef 
 function MouseOver()   
 	for i=1,10 do
 		local button =_G["MerchantItem"..i.."ItemButton"]
-		if(MouseIsOver(button)) then ToolTip() end
+		if(MouseIsOver(button)) then ToolTip(i) end
    end
 	for i=1,5 do -- bag support by Szyler
 		for j=1,36 do
